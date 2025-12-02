813b40c3bfae8db51ddfb948cec3253439eb0946
Gabriel Lima dos Santos <gabriellimadossantos1993@gmail.com>
2025-12-02 19:31:39 -0300

    ajuste projetos

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,26 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from '../pages/LoginPage';
-import SignupPage from '../pages/SignupPage';
-import TasksPage from '../pages/TasksPage';
+// src/routes/AppRoutes.jsx
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import TasksPage from "../pages/TasksPage";
+import ProjectsStatusPage from "../pages/ProjectsStatusPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+
+        {/* Tela principal (tarefas + sidebar) */}
         <Route path="/app" element={<TasksPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
+
+        {/* Nova tela estilo Notion para status de projetos */}
+        <Route path="/projects/status" element={<ProjectsStatusPage />} />
+
+        {/* Qualquer outra rota cai para login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
